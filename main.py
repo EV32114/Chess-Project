291b92c7e67c7607861536379fe017594460ce62
yoavarviv <yoavarviv1@gmail.com>
2021-06-10 13:30:28 +0300

    100% done

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.filters import threshold_otsu
-# we create the before-change chessboard.
 import crop
-global chessBoard = np.array([["R", "N", "B", "K", "Q", "B", "N", "R"],
+
+# default chess board (before changes).
+chessBoard = np.array([["R", "N", "B", "K", "Q", "B", "N", "R"],
                       ["P", "P", "P", "P", "P", "P", "P", "P"],
                       ["", "", "", "", "", "", "", ""],
                       ["", "", "", "", "", "", "", ""],
@@ -13,7 +14,8 @@ global chessBoard = np.array([["R", "N", "B", "K", "Q", "B", "N", "R"],
                       ["", "", "", "", "", "", "", ""],
                       ["p", "p", "p", "p", "p", "p", "p", "p"],
                       ["r", "n", "b", "q", "k", "b", "n", "r"]])
-global updatedChess = np.zeros((8,8))
+# array converying the empty spaces.
+updatedChess = np.zeros((8,8))
 
 vid = cv2.VideoCapture(0)
 
@@ -29,21 +31,14 @@ refPt = crop.poggers(frame)
 # # Threshold for an optimal value, it may vary depending on the image.
 # frame[dst > 0.01 * dst.max()] = [0, 0, 255]
 # cv2.imshow('dst', frame)
-
-# Display the resulting frame
-
-
-
-
 # img = cv2.imread('yes.jpg') # we read the image
-
 
 # Round to next smaller multiple of 8
 # https://www.geeksforgeeks.org/round-to-next-smaller-multiple-of-8/
 def round_down_to_next_multiple_of_8(a):
     return a & (-8) # rounds down to the next multiple of 8 using the algorithm above.
 
-# we convert the numbers to a proper chess position
+# we convert the numbers to a proper chess position for printing.
 def convert(x, y):
     strY = chr(97 + y)
     returnStr = strY + str(8-x)
@@ -64,6 +59,7 @@ def blurImg(img):
     # we blur the image.
     img = cv2.blur(img, (5, 5))
     return img
+
 # we iterate over the tiles, and count unique colors inside
 # we use the following algorithm:
 # https://stackoverflow.com/a/56606457/11089932
@@ -81,12 +77,25 @@ def getUniqueColors(img):
 # we mask empty squares using cutoff from Otsu's method
 # reference: https://en.wikipedia.org/wiki/Otsu%27s_method
 def threshAndMask(count_unique_colors):
+    # we can decrease the val to get more accurate results, however, 
+    # this can only work granted we use pieces that don't match the dark board.
+    # therefore, we should most likely refrain from using this unless testing and seeing
+    # reliability.
     val = threshold_otsu(count_unique_colors)
     mask = count_unique_colors < val
     return mask
 
+def mark(out, mask, wh_t):
+    for x in np.arange(8):
+        for y in np.arange(8):
+            if mask[y, x]:
+                cv2.rectangle(out, (x * wh_t + 3, y * wh_t + 3),
+                              ((x + 1) * wh_t - 3, (y + 1) * wh_t - 3), (255, 0, 0), 2)  # just for visuals, it surrounds the square.
+
 def updateBoardAndMark(out, mask, wh_t):
     # we put more visualization output, although we can already find the empty tiles.
+    # however, in this function we also update our chess board using 0`s and 1`s where 
+    # 1 signifies an empty space while 0 signifies a space that's taken.
     for x in np.arange(8):
         for y in np.arange(8):
             if mask[y, x]:
@@ -96,13 +105,14 @@ def updateBoardAndMark(out, mask, wh_t):
 
             print(updatedChess[y,x])
     return out
+
 #plt.subplot(1, 3, 2), plt.imshow(count_unique_colors, cmap='gray')
 #plt.subplot(1, 3, 3), plt.imshow(out)
 #plt.tight_layout(), plt.show()
 # now, we have an array called updatedChess which holds the chess board.
 # all we need to do now is compare the two arrays, and see which piece moved.
 
-def printMove()
+def printMove():
     global updatedChess
     global chessBoard
     string = ""
@@ -123,27 +133,48 @@ def printMove()
 
 
 def main():
-    vid = cv2.VideoCapture(0)
-    while True:
+    vid = cv2.VideoCapture(0) # we turn on the camera.
+    while True:     
+        # this while true will eventually have a breakpoint, it will break when
+        # the game is over.
         ret, frame = vid.read()
-        frame = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        # we have previously discovered our reference points, these are the points 
+        # we need to crop our image to in order to find the chessboard and get the best
+        # picture of it. As so, in this line we crop the image to our desired area.
+        frame = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]] 
+        # we resize and blur the image.
         frame = resizeImage(frame)
         frame = blurImg(frame)
+        # we get an array, or, more accurately an image representation of the unique
+        # colors in the frame. 
         count_unique_colors, wh_t = getUniqueColors(frame)
+        # we threshold using Otsu's method and mask the recieved count of unique colors.
         mask = threshAndMask(count_unique_colors)
-        frame = updateBoardAndMark(frame, mask, wh_t)
-        cv2.imshow('frame', frame)
-        printMove()
+        mark(frame, mask, wh_t)
+        cv2.imshow('frame', cv2.flip(frame, 1))
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-    
-    cv2.imshow('frame', frame)
+        if key == ord('q'): 
+            # this key click means the player had played his turn and we can scan the
+            # board. Therefore, we will now scan the board again, find the differences
+            # between the array of the start of the turn, and the aftermath, and display
+            # what we found the move was. 
+            ret, frame = vid.read()
+            frame = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+            frame = resizeImage(frame)
+            frame = blurImg(frame)
+            count_unique_colors, wh_t = getUniqueColors(frame)
+            mask = threshAndMask(count_unique_colors)
+            # and we update the board and mark the free spaces.
+            frame = updateBoardAndMark(frame, mask, wh_t)
+            # we show the frame we got.
+            cv2.imshow('frame', cv2.flip(frame, 1))
+            # we print the final move.
+            printMove()
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__ == "__main__";
+if __name__ == "__main__":
     main()
 
 
