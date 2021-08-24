400385cbadbfeae4e5cfbcc37881432c8a8a5355
yoavarviv <yoavarviv1@gmail.com>
2021-08-24 13:46:03 +0300

    save changes

--- Arduino-Chess/Arduino-Chess.ino
+++ Arduino-Chess/Arduino-Chess.ino
@@ -2,21 +2,21 @@ const int BUTTON_PIN = 2;
 
 void setup() {
   Serial.begin(9600); // setting baud rate to 9600.
-  pinMode(BUTTON_PIN, INPUT); // setting the button pin as an input.
+  // pinMode(BUTTON_PIN, INPUT); // setting the button pin as an input.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  
+  /**
   int sensorVal = digitalRead(2);
   if(sensorVal == 1){
     // if sensor val is 1, the button was pressed, 
     // and we send the appropriate data to the serial port.  
     Serial.println("H");
   }
+ 
+  **/
   
-  
-  /**
   if(Serial.available()){
     char inByte = Serial.read();
     if(inByte == 'H'){
@@ -27,6 +27,6 @@ void loop() {
     }
   }
   delay(1);
-  **/
+  
 
 }
