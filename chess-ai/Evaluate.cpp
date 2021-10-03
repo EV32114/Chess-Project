9ac89c293d9a00fb5e3ec0a5a41cd4946c580af3
yoavarviv <yoavarviv1@gmail.com>
2021-10-03 13:27:10 +0300

    for ev

--- chess-ai/Evaluate.cpp
+++ chess-ai/Evaluate.cpp
@@ -1,4 +1,6 @@
 #include "Evaluate.h"
+#include <vector>
+#include <iterator>
 
 std::vector<std::string> blackMoves;
 std::vector<std::string> whiteMoves;
@@ -10,21 +12,17 @@ int Evaluate::evalPos(string pos)
 	int blackValid[MAX_VALID_MOVES] = { 0 };
 	int whiteValid[MAX_VALID_MOVES] = { 0 };
 	
-	countPieces(pos, whitePieceCounter, blackPieceCounter);
-
-	int evalWhite = evaluatePiecesMidGame(whitePieceCounter, blackPieceCounter, true);
-	int evalBlack = evaluatePiecesMidGame(whitePieceCounter, blackPieceCounter, false);
-	cout << "White's evaluation: \n" << evalWhite << endl;
-	cout << "Black's evaluation: \n" << evalBlack << endl;
-
-	evalWhite = evaluatePiecesEndGame(whitePieceCounter, blackPieceCounter, true);
-	evalBlack = evaluatePiecesEndGame(whitePieceCounter, blackPieceCounter, false);
-	cout << "White's evaluation: \n" << evalWhite << endl;
-	cout << "Black's evaluation: \n" << evalBlack << endl;
+	
+	//countPieces(pos, whitePieceCounter, blackPieceCounter);
 
 	Board board(pos);
-	board.printBoard();
 	getValidMoves(board);
+	for (std::vector<std::string>::iterator i = blackMoves.begin(); i != blackMoves.end(); i++) {
+		cout << *i << endl;
+	}
+	for (std::vector<std::string>::iterator i = whiteMoves.begin(); i != whiteMoves.end(); i++) {
+		cout << *i << endl;
+	}
 	return 0;
 }
 
@@ -91,93 +89,51 @@ void Evaluate::getValidMoves(Board& board) {
     }
 }
 
-int Evaluate::evaluatePiecesMidGame(int *whitePieces, int *blackPieces, bool white) {
+int Evaluate::evaluatePiecesBlack(int *blackPieces, bool midGame) {
 	int posEval = 0;
 
 	for (int i = 0; i < 6; i++) {
-		if (white) {
-			switch (i) {
-			case BISHOP:
-				posEval += whitePieces[BISHOP] * MID_BISHOP;
-				break;
-			case ROOK:
-				posEval += whitePieces[ROOK] * MID_ROOK;
-				break;
-			case PAWN:
-				posEval += whitePieces[PAWN] * MID_PAWN;
-				break;
-			case QUEEN:
-				posEval += whitePieces[QUEEN] * MID_QUEEN;
-				break;
-			case KNIGHT:
-				posEval += whitePieces[KNIGHT] * MID_KNIGHT;
-				break;
-			}
-		}
-		else {
-			switch (i) {
-			case QUEEN:
-				posEval += blackPieces[QUEEN] * MID_QUEEN;
-				break;
-			case KNIGHT:
-				posEval += blackPieces[KNIGHT] * MID_KNIGHT;
-				break;
-			case PAWN:
-				posEval += blackPieces[PAWN] * MID_PAWN;
-				break;
-			case ROOK:
-				posEval += blackPieces[ROOK] * MID_ROOK;
-				break;
-			case BISHOP:
-				posEval += blackPieces[BISHOP] * MID_BISHOP;
-				break;
-			}
+		switch (i) {
+		case BISHOP:
+			posEval += blackPieces[BISHOP] * midGame ? MID_BISHOP : END_BISHOP;
+			break;
+		case ROOK:
+			posEval += blackPieces[ROOK] * midGame ? MID_ROOK : END_ROOK;
+			break;
+		case PAWN:
+			posEval += blackPieces[PAWN] * midGame ? MID_PAWN : END_PAWN;
+			break;
+		case QUEEN:
+			posEval += blackPieces[QUEEN] * midGame ? MID_QUEEN : END_QUEEN;
+			break;
+		case KNIGHT:
+			posEval += blackPieces[KNIGHT] * midGame ? MID_KNIGHT : END_KNIGHT;
+			break;
 		}
 	}
 	return posEval;
 }
 
-int Evaluate::evaluatePiecesEndGame(int* whitePieces, int* blackPieces, bool white) {
+int Evaluate::evaluatePiecesWhite(int* whitePieces, bool midGame) {
 	int posEval = 0;
 
 	for (int i = 0; i < 6; i++) {
-		if (white) {
-			switch (i) {
+		switch (i) {
 			case BISHOP:
-				posEval += whitePieces[BISHOP] * END_BISHOP;
+				posEval += whitePieces[BISHOP] * midGame ? MID_BISHOP : END_BISHOP;
 				break;
 			case ROOK:
-				posEval += whitePieces[ROOK] * END_ROOK;
+				posEval += whitePieces[ROOK] * midGame ? MID_ROOK : END_ROOK;
 				break;
 			case PAWN:
-				posEval += whitePieces[PAWN] * END_PAWN;
+				posEval += whitePieces[PAWN] * midGame ? MID_PAWN : END_PAWN;
 				break;
 			case QUEEN:
-				posEval += whitePieces[QUEEN] * END_QUEEN;
+				posEval += whitePieces[QUEEN] * midGame ? MID_QUEEN : END_QUEEN;
 				break;
 			case KNIGHT:
-				posEval += whitePieces[KNIGHT] * END_KNIGHT;
+				posEval += whitePieces[KNIGHT] * midGame ? MID_KNIGHT : END_KNIGHT;
 				break;
-			}
-		}
-		else {
-			switch (i) {
-			case QUEEN:
-				posEval += blackPieces[QUEEN] * END_QUEEN;
-				break;
-			case KNIGHT:
-				posEval += blackPieces[KNIGHT] * END_KNIGHT;
-				break;
-			case PAWN:
-				posEval += blackPieces[PAWN] * END_PAWN;
-				break;
-			case ROOK:
-				posEval += blackPieces[ROOK] * END_ROOK;
-				break;
-			case BISHOP:
-				posEval += blackPieces[BISHOP] * END_BISHOP;
-				break;
-			}
 		}
 	}
 	return posEval;
