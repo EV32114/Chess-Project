2de51f0b8270305d766610afb15ac60bccf571e1
yoavarviv <58586004+yoavarviv@users.noreply.github.com>
2021-10-17 12:49:16 +0300

    Update King.cpp

--- Chess-AI/King.cpp
+++ Chess-AI/King.cpp
@@ -35,12 +35,6 @@ bool King::isValidPieceMove(int* src, const int* dest, const Board& board) const
 }
 
 std::vector<std::string> King::getValidMoves(int* src, const Board& board) const {
-    // A king only has 8 squares to move. We'll check which squares around him are free and return an array consisting of them.
-    bool isWhite = isupper(this->_type);
-    int sign = isWhite ? 1 : -1;
-    std::vector<std::string> validMoves;
-    int nIndex = 0;
-   
     /*
         This for loop goes over each possible move for the king and checks whether it is a valid move. If so, it adds it to the valid move array.
     */
@@ -49,17 +43,15 @@ std::vector<std::string> King::getValidMoves(int* src, const Board& board) const
             if (!(i == 0 && j == 0)) {
                 if (src[0] - i >= 0 && src[1] - j >= 0) {
                     if (board.getBoard()[src[0] - i][src[1] - j]->getType() == EMPTY_SQUARE ||
-                        ((isupper(board.getBoard()[src[0] - i][src[1] - j]->getType()) && !isWhite) || (islower(board.getBoard()[src[0] - i][src[1] - j]->getType()) && isWhite))) {
+                        ((isupper(board.getBoard()[src[0] - i][src[1] - j]->getType()) && !_isWhite) || (islower(board.getBoard()[src[0] - i][src[1] - j]->getType()) && _isWhite))) {
                         validMoves.push_back(std::to_string(src[0] - i) + std::to_string(src[1] - j));
-                        nIndex++;
                     }
                 }
                 
                 if (src[0] + i < 8 && src[1] + j < 8) {
                     if (board.getBoard()[src[0] + i][src[1] + j]->getType() == EMPTY_SQUARE ||
-                        ((isupper(board.getBoard()[src[0] + i][src[1] + j]->getType()) && !isWhite) || (islower(board.getBoard()[src[0] + i][src[1] + j]->getType()) && isWhite))) {
+                        ((isupper(board.getBoard()[src[0] + i][src[1] + j]->getType()) && !_isWhite) || (islower(board.getBoard()[src[0] + i][src[1] + j]->getType()) && _isWhite))) {
                         validMoves.push_back(std::to_string(src[0] + i) + std::to_string(src[1] + j));
-                        nIndex++;
                     }
                 }
             }
